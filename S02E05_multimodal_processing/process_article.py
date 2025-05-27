f584faf54c1a49adb86a47c4de176ada94532edf
ematrejek <147256863+ematrejek@users.noreply.github.com>
2025-05-28 00:12:47 +0200

    Update process_article.py

--- S02E05_multimodal_processing/process_article.py
+++ S02E05_multimodal_processing/process_article.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 # Konfiguracja
-API_KEY = "8eed1983-ee32-479e-8c44-eb85077a62e8"
+API_KEY = "xxx" ## API do platformy AI devs
 ARTICLE_URL = "https://c3ntrala.ag3nts.org/dane/arxiv-draft.html"
 QUESTIONS_URL = f"https://c3ntrala.ag3nts.org/data/{API_KEY}/arxiv.txt"
 
